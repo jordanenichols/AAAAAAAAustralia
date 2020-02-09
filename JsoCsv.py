bd73094f540380eeb07755a126f909d89e9ba9bf
William <wkopans123@gmail.com>
2020-02-08 20:18:36 -0500

    Update JsoCsv.py
    run using JsoCsv.py --file data.json
    
    Make sure everything is in the same directory!

--- JsoCsv.py
+++ JsoCsv.py
@@ -3,11 +3,9 @@ import csv
 import argparse
 from pprint import pprint
 
-
 parser = argparse.ArgumentParser(description='Convert a CSV to JSON.')
 parser.add_argument('--file', type=str, help='The filename you want converted')
 args = parser.parse_args()
-
 
 """
 from JsoCsv import convert_to_csv_from_json
@@ -18,8 +16,10 @@ def convert_to_csv_from_json(json_file, output_file):
             j = json.loads(f.read())
             pprint(j)
             csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(j["headers"])
+            csv_writer.writerow(["month", "number"])
             for d in j["data"]:
                 csv_writer.writerow(d)
 
 convert_to_csv_from_json(args.file, args.file.replace("json", "csv"))
+
+#python3 JsoCsv.py --file test.json
